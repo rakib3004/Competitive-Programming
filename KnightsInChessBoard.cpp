923d6a4da8dfaace94c90e53565cf31342e82a3e
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-03-28 20:00:31 +0600

    add template code in KnightsInChessBoard

---
+++ KnightsInChessBoard.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test(){
+
+int m,n;
+
+cin>>m>>n;
+
+int i,j;
+
+
+for(i=1;i<m)
+
+}
+
+int main(){
+
+
+int t;
+
+cin>>t;
+
+
+while(t--){
+
+    test();
+}
+
+
+return 0;
+
+}
