b94773f02ba9404f811a9d1473b78947905f6091
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-07 12:13:55 +0600

    match substrings istream

---
+++ SubstringFrequency.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+
+string a,b;
+
+cin>>a>>b;
+
+
+int m=a.size();
+int n= b.size();
+
+int i;
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        test();
+
+    }
+
+
+    return 0;
+
+}
