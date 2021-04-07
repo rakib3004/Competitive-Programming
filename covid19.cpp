715eb79701ebfcb8299cbb62e904abe371bd347d
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-07 16:06:53 +0600

    codid 19 distance making code

---
+++ covid19.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+
+
+    return;
+
+}
+int main()
+{
+
+//
+//    int t;
+//
+//    cin>>t;
+//
+//    while(t--)
+//    {
+//
+//        test();
+//
+//    }
+
+
+
+int n;
+
+
+
+cin>>n;
+
+
+cout<<(n/6)+1<<endl;
+
+    return 0;
+
+}
