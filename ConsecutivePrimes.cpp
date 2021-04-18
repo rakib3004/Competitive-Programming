7f2af1a270f1d9dc652fb6b6007ce81de1fceec3
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-04-19 05:45:35 +0600

    add template in prime problem

---
+++ ConsecutivePrimes.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test(int it)
+{
+    int n;
+
+
+    cin>>n;
+
+
+
+
+
+    cout<<"Case #"<<it<<": ";
+
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+
+    int it =0;
+    while(t--)
+    {
+        it++;
+        test(it);
+        cout<<endl;
+    }
+
+
+    return 0;
+
+}
