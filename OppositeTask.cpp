669b2b77850bba261c62a8563fd63e07653ac549
rakib3004 <bsse1129@iit.du.ac.bd>
2021-06-25 20:07:26 +0600

    add Opposite task problem

---
+++ OppositeTask.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+
+
+int t;
+
+
+cin>>t;
+
+
+
+while(t--){
+        int n;
+
+
+cin>>n;
+
+
+int a= n/2;
+
+
+cout<<a<<" "<<n-a<<endl;
+
+}
+
+
+
+    return 0;
+
+}
