ccade4dd8226929bdb2e78610250eca30ee1dde0
rakib3004 <bsse1129@iit.du.ac.bd>
2022-01-23 19:24:47 +0600

    add east west north south street

---
+++ EastWestSouthNorthRoad.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+
+
+    return;
+
+}
+int main()
+{
+
+
+    int t=1;
+
+    int m,n;
+
+    cin>>m>>n;
+
+
+    cout<<(m-1)*(n-1)<<endl;
+
+
+    return 0;
+
+}
