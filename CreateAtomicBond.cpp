3d784c49fcdb160948a3da94e236760b80997819
rakib3004 <bsse1129@iit.du.ac.bd>
2022-01-23 20:52:15 +0600

    find atomic bond sequence using writing codes

--- CreateAtomicBond.cpp
+++ CreateAtomicBond.cpp
@@ -76,39 +76,81 @@ void test()
 
 
     cin>>a>>b>>c;
-int d = findLargest(a,b,c);
-int e = findSecondLargest(a,b,c);
-
-int f1= min(a,b);
-int f2= min(f1,c);
-int f= min(f1,f2);
 
 
-int m = d-e;
+    int d = findLargest(a,b,c);
+    int e = findSecondLargest(a,b,c);
 
-int d1 = d;
-int e1 = e;
+    int f1= min(a,b);
+    int f2= min(f1,c);
+    int f= min(f1,f2);
 
-bool checkEquality=false;
-
-while(checkEquality==false){
-if(f-m==0){
-    checkEquality=true;
-}
-else{
-    m=m+2;
-    d--;
-    e--;
-checkEquality=false;
-}
+    if(f+e<d||(a+b+c)%2==1){
+       cout<<"Impossible"<<endl;
+       return;
+    }
 
 
-}
-int commondBigBond = min(d,e);
-int oneOtherBond = d1-commondBigBond;
-int anotherBond = e1-commondBigBond;
+    int m = d-e;
 
-cout<<commondBigBond<<" "<<oneOtherBond<<" "<<anotherBond<<" "<<endl;
+    int d0 = d;
+    int e0 = e;
+    int f0 =f;
+
+    bool checkEquality=false;
+
+    while(checkEquality==false)
+    {
+        if(f-m==0)
+        {
+            checkEquality=true;
+        }
+        else
+        {
+            m=m+2;
+            d--;
+            e--;
+            checkEquality=false;
+        }
+
+
+    }
+    int ax = min(d,e);
+    int ay = e0-ax;
+    int az = d0-ax;
+
+
+    if(a==d0&&b==e0)
+    {
+        cout<<ax<<" "<<ay<<" "<<az<<" "<<endl;
+
+    }
+
+    else if(b==d0&&a==e0)
+    {
+        cout<<ax<<" "<<az<<" "<<ay<<" "<<endl;
+
+    }
+    else  if(a==f&&b==d0)
+    {
+        cout<<az<<" "<<ax<<" "<<ay<<" "<<endl;
+
+    }
+    else  if(a==f&&b==e0)
+    {
+        cout<<ay<<" "<<ax<<" "<<az<<" "<<endl;
+
+    }
+    else  if(a==d0&&b==f)
+    {
+        cout<<az<<" "<<ay<<" "<<ax<<" "<<endl;
+
+    }
+    else  if(a==e0&&b==f)
+    {
+        cout<<ay<<" "<<az<<" "<<ax<<" "<<endl;
+
+    }
 
 
 
