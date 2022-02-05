a3cd76450530a34510ee627fde895af996e52ddb
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-06 01:13:02 +0600

    add kick start formate for coding

---
+++ KickStart/2018/RoundA/EvenDigits.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+         long long int n;
+
+
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        test();
+
+    }
+
+
+    return 0;
+
+}
