809ca19e043b270ba3a0042c6f43747eea4a4cb9
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-06 13:06:29 +0600

    add even digits codes

--- KickStart/2018/RoundA/EvenDigits.cpp
+++ KickStart/2018/RoundA/EvenDigits.cpp
@@ -1,11 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int caseI;
+bool checkBeautiful(int n){
 
+int number;
+
+ while(n!=0){
+
+     int a=n%10;
+     if(a%2==0){
+         n=n/10;
+     }
+     else{
+
+         return false;
+     }
+ }
+
+ return true;
+
+}
 void test()
 {
-         long long int n;
+    caseI++;
+         int n,a;
 
+         cin>>n;
+
+        int i=0;
+
+        int left, right;
+
+        for(; i<n;i++){
+            if(checkBeautiful(n+i)){
+                right=i;
+                break;
+            }
+
+        }
+
+         for(; i<n;i++){
+            if(checkBeautiful(n-i)){
+                left=i;
+                break;
+            }
+
+        }
+
+        int result= min(left,right);
+
+        cout<<"case #"<<caseI<<": "<<result<<endl;
 
 
     return;
@@ -13,7 +58,7 @@ void test()
 }
 int main()
 {
-
+caseI=0;
 
     int t;
 
