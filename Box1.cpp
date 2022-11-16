5f790cf58f50d8f0d738bb43c5136870c1d6d2df
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-11-16 12:19:31 +0600

    print square box

---
+++ Box1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+long long int n;
+cin>>n;
+
+int i,j;
+
+for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+        printf("\n");
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+    while(t--)
+    {
+        test();
+
+
+
+    }
+
+
+    return 0;
+
+}
+
+
