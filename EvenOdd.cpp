e7cfd78cf56dc2f08760b9364ec03da5ef2ba51a
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-11-16 11:01:33 +0600

    add codebase of even odd

---
+++ EvenOdd.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+long long int n;
+cin>>n;
+
+
+if(n%2){
+    cout<<"odd"<<endl;
+}
+
+else{
+    cout<<"even"<<endl;
+}
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+    while(t--)
+    {
+        test();
+    }
+
+
+    return 0;
+
+}
