21417be0ede69309494c0279f1b027f433d33c1d
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-11-16 12:33:59 +0600

    print string length

---
+++ Summation.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+int n;
+cin>>n;
+
+int i,j;
+int sum=0;
+
+sum=(n/10000)+(n%10);
+cout<<"Sum = "<<sum<<endl;
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+    while(t--)
+    {
+        test();
+
+
+
+    }
+
+
+    return 0;
+
+}
+
+
