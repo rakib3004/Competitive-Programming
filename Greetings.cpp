740aabfe033a93134131e33fbfb4a723a939b180
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-12-05 20:50:52 +0600

    add greetings code

---
+++ Greetings.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test(int i)
+{
+    int a,b;
+
+
+    cin>>a>>b;
+    cout<<"Case "<<i<<": ";
+    cout<<a+b;
+    printf("\n");
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+
+    int i=0;
+
+    while(t--)
+    {
+
+        i++;
+
+        test(i);
+
+
+
+    }
+
+
+    return 0;
+
+}
+
+
