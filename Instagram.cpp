7a6b50316ddc614b1af7f96cc17697d3693d6e4f
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-12-28 20:49:24 +0600

    add 4 new code for code-chef programming contest

---
+++ Instagram.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+int x,y;
+
+
+cin>>x>>y;
+
+
+if(x-y>=10){
+    cout<<"YES"<<endl;
+}
+else{
+    cout<<"NO"<<endl;
+}
+
+}
+
+
+int main()
+{
+
+ int t;
+    cin>>t;
+
+
+    while(t--)
+    {
+        test();
+    }
+
+
+    return 0;
+
+}
