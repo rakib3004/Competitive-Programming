4160aba51a0908f03f5ca05870239da3ce7a6add
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-08-12 18:30:01 +0600

    add code template

---
+++ samsung/sample.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl    '\n'
+#define all(v)   v.begin(), v.end()
+#define rep(i, s, n)  for(int i = s; i <= n; i++)
+#define rev(i, n, s)  for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*****User defined function*****/
+
+void module() {
+
+}
+
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << fixed << showpoint;
+    cout << setprecision(15);
+
+    int test = 1;
+    // cin >> test;
+    while(test--) module();
+
+    return 0;
+}
