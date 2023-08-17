733513897c94539aa61cadf2c7ecca4e27262d7c
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-08-17 14:58:55 +0600

    add binrary tree problem

---
+++ samsung/binary-tree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    return newNode;
+}
+
+void insert(Node*& root, int data) {
+    if (root == nullptr) {
+        root = createNode(data);
+        return;
+    }
+
+    if (data <= root->data) {
+        insert(root->left, data);
+    } else {
+        insert(root->right, data);
+    }
+}
+
+void inorderTraversal(Node* root) {
+    if (root == nullptr) return;
+    inorderTraversal(root->left);
+    std::cout << root->data << " ";
+    inorderTraversal(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    insert(root, 5);
+    insert(root, 3);
+    insert(root, 8);
+    insert(root, 2);
+    insert(root, 4);
+    insert(root, 7);
+    insert(root, 9);
+
+    std::cout << "Inorder Traversal: ";
+    inorderTraversal(root);
+    std::cout << std::endl;
+
+    return 0;
+}
